8c6c5178af0c107a853ba6099bcf98a2b6888289
bolajeee <ibrahimomobolaji1999@gmail.com>
2023-06-30 15:23:09 +0100

    omo, still not working.

--- index.js
+++ index.js
@@ -27,6 +27,17 @@ $(function() {
     });
 });
 
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 50) {
+            $(".header").addClass("active");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+           $(".header").removeClass("active");
+        }
+    });
+});
+
 
 const sr = ScrollReveal({
   distance: '65px',
